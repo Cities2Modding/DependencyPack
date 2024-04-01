a786eeed3d91e31b2301aa92ca88d96bebb278be
Dan <12039578+optimus-code@users.noreply.github.com>
2024-04-01 02:11:25 +0100

    wip

---
+++ DependencyPack/Mod.cs
@@ -0,0 +1,29 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using HarmonyLib;
+using Unity.Entities;
+
+namespace DependencyPack
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger( $"{nameof( DependencyPack )}.{nameof( Mod )}" ).SetShowsErrorsInUI( false );
+
+        public void OnLoad( UpdateSystem updateSystem )
+        {
+            InitialiseHarmony( );
+        }
+
+        public void OnDispose( )
+        {
+        }
+
+        private void InitialiseHarmony( )
+        {
+            var harmony = new Harmony( "cities2modding.harmonyx_lib" );
+            log.Info( $"DependencyPack has loaded Harmony" );
+        }
+    }
+}
